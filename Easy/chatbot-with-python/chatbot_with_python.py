22f22f7a93e26260c94aefb354a6fedd7afb4b63
Roberto Keppler <rkeppler42@gmail.com>
2025-07-27 02:59:18 -0300

    feat(easy): add Simple Chatty Bot Task 4 – count to user-specified number
    Implements a for-loop to count from 0 to a user-defined number. Updates README with task description, formatted instructions, and example output.

--- Easy/chatbot-with-python/chatbot_with_python.py
+++ Easy/chatbot-with-python/chatbot_with_python.py
@@ -10,14 +10,24 @@ def remind_name():
 
 def guess_age():
     print('Let me guess your age.')
-    print('Enter remainders of dividing your age by 3, 5 and 7.')
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
     remainder_of_3 = int(input())
     remainder_of_5 = int(input())
     remainder_of_7 = int(input())
     your_age = (remainder_of_3 * 70 + remainder_of_5 * 21 + remainder_of_7 * 15) % 105
     print(f"Your age is {your_age}; that's a good time to start programming!")
 
+def counting():
+    print("Now I will prove to you that I can count to any number you want.")
+    number = int(input())
+    counter = 0
+    while counter <= number:
+        print(counter, "!")
+        counter += 1
+    print("Completed, have a nice day!")
+
 
 greet("Aid", 2025)
 remind_name()
 guess_age()
+counting()
