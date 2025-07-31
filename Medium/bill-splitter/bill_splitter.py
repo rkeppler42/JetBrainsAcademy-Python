9b37e00aa687869a17caf06166007e13105a947a
Roberto Keppler <rkeppler42@gmail.com>
2025-07-31 20:48:33 -0300

    feat: implement Task 2 (Split the Bill) and update README

--- Medium/bill-splitter/bill_splitter.py
+++ Medium/bill-splitter/bill_splitter.py
@@ -11,7 +11,12 @@ if number_of_friends > 0:
         name_of_friend = input()
         dictionary_of_friends[name_of_friend] = 0
         counter += 1
-
+    print()
+    print("Enter the total bill value:")
+    total_bill_value = float(input())
+    price_per_person = round(total_bill_value / number_of_friends, 2)
+    for person in dictionary_of_friends:
+        dictionary_of_friends[person] = price_per_person
     print(dictionary_of_friends)
 else:
     print("No one is joining for the party")
