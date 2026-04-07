591b662f95f8a9cb16bafb379ad5c283fbd582cb
Roberto Keppler <rkeppler42@gmail.com>
2026-04-07 17:23:22 -0300

    feat(easy): add the final code for the project Last Pencil

--- Easy/last-pencil/last_pencil.py
+++ Easy/last-pencil/last_pencil.py
@@ -1,23 +1,42 @@
+import random
+from typing import Literal
+
+
+class InvalidNumberOfPencils(Exception):
+    pass
+
 def number_of_pencils() -> int:
     """Asks the user to enter how many pencils will be used in the game.
 
     :return: the number of pencils that will be used in the game.
     """
     print("How many pencils would you like to use:")
-    num_of_pencils = int(input())
-    return num_of_pencils
+    while True:
+        try:
+            num_pencils = int(input())
+            if num_pencils < 0:
+                raise ValueError
+            if num_pencils == 0:
+                raise InvalidNumberOfPencils
+            break
+        except ValueError:
+            print("The number of pencils should be numeric")
+        except InvalidNumberOfPencils:
+            print("The number of pencils should be positive")
+    return num_pencils
 
 
-def define_first_player() -> list:
+
+def define_first_player() -> Literal["John", "Jack"]:
     """Defines the first player who will play the game.
 
-    :return: a list of players where list[0] is the first player and the list[1] is the second player.
+    :return: returns the first player who will play the game.
     """
     print("Who will be the first (John, Jack):")
     first_player = input()
-    if first_player == "John":
-        return ["John", "Jack"]
-    return ["Jack", "John"]
+    while first_player not in ["John", "Jack"]:
+        first_player = input("Choose between John and Jack:\n")
+    return first_player
 
 
 def print_pencils(num_pencils: int) -> None:
@@ -25,41 +44,84 @@ def print_pencils(num_pencils: int) -> None:
 
     :param num_pencils: the number of pencils remaining in the game.
     """
-    print("|" * num_pencils)
+    if num_pencils > 0:
+        print("|" * num_pencils)
 
 
-def print_player_turn(player_order: list, player_count: int) -> None:
-    """Prints the player who will play the game in this turn.
+def winner(who_wins: str) -> None:
+    """Prints the winner of the game.
 
-    :param player_order: the players in order of play.
-    :param player_count: the player who will play the game in this turn.
+    :param who_wins: the player who wins.
     """
-    if player_count % 2 == 0:
-        print(f"{player_order[0]}'s turn:")
-    else:
-        print(f"{player_order[1]}'s turn:")
+    print(f"{who_wins} won!")
+
+
+def player_turn(num_of_pencils: int) -> tuple[int, int]:
+    """Validates the player's move and if valid makes the move.
+
+    :param num_of_pencils: the number of pencils remaining in the game.
+    :return: the number of pencils remaining in the game after the player's turn and how many pencils were taken.
+    """
+
+    while True:
+        player_move = input()
+        if player_move not in ['1', '2', '3']:
+            print("Possible values: '1', '2' or '3'")
+        elif int(player_move) > num_of_pencils:
+            print("Too many pencils were taken")
+        else:
+            return num_of_pencils - int(player_move), int(player_move)
 
 
 
-def main_game_loop(num_pencils: int, player_order: list) -> None:
+def bot_turn(num_of_pencils: int) -> tuple[int, int]:
+    """Emulates the bot's turn.
+
+    :param num_of_pencils: the number of pencils remaining in the game.
+    :return: the number of pencils remaining in the game after the bot's turn and how many pencils the bot took.
+    """
+    if num_of_pencils % 4 == 0:
+        bot_move = 3
+        return num_of_pencils - bot_move, bot_move
+    if num_of_pencils % 4 == 3:
+        bot_move = 2
+        return num_of_pencils - bot_move, bot_move
+    if num_of_pencils % 4 == 2:
+        bot_move = 1
+        return num_of_pencils - bot_move, bot_move
+    if num_of_pencils == 1:
+        bot_move = 1
+        return num_of_pencils - bot_move, bot_move
+    bot_move = random.randint(1, 3)
+    return num_of_pencils - bot_move, bot_move
+
+
+def main_game_loop(num_pencils: int, first_player: str) -> None:
     """The main game loop.
 
     :param num_pencils: the number of pencils remaining in the game.
-    :param player_order: the list of players who will play the game.
+    :param first_player: the player who will play the game first.
     """
-    player_count = 0
+
+    print_pencils(num_pencils)
+    who_plays = first_player
     while num_pencils > 0:
+        print(f"{who_plays}'s turn!")
+        if who_plays == "John":
+            num_pencils, player_move = player_turn(num_pencils)
+            who_plays = "Jack"
+        elif who_plays == "Jack":
+            num_pencils, bot_move = bot_turn(num_pencils)
+            print(bot_move)
+            who_plays = "John"
         print_pencils(num_pencils)
-        print_player_turn(player_order, player_count)
-        player_move = int(input())
-        num_pencils -= player_move
-        player_count += 1
+    winner(who_plays)
 
 
 def main():
     n_of_pencils = number_of_pencils()
-    player_order = define_first_player()
-    main_game_loop(n_of_pencils, player_order)
+    first_player = define_first_player()
+    main_game_loop(n_of_pencils, first_player)
 
 
 if __name__ == "__main__":
