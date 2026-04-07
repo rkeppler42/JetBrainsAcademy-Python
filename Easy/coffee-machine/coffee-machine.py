ee65328e5aa39a8eaa87e9afd33c32d323f20cf3
Roberto Keppler <rkeppler42@gmail.com>
2026-04-07 19:59:52 -0300

    feat(easy): add the code that fulfills the second stage of the project

--- Easy/coffee-machine/coffee-machine.py
+++ Easy/coffee-machine/coffee-machine.py
@@ -1,8 +1,12 @@
-# Write your code here
-print("Starting to make a coffee")
-print("Grinding coffee beans")
-print("Boiling water")
-print("Mixing boiled water with crushed coffee beans")
-print("Pouring coffee into the cup")
-print("Pouring some milk into the cup")
-print("Coffee is ready!")
+water_per_coffee = 200
+milk_per_coffee = 50
+coffee_beans_per_coffee = 15
+
+
+print("Write how many cups of coffee you will need:")
+num_cups_of_coffee = int(input())
+
+print(f"For {num_cups_of_coffee} cups of coffee you will need:")
+print(f"{int(num_cups_of_coffee * water_per_coffee)} ml of water")
+print(f"{int(num_cups_of_coffee * milk_per_coffee)} ml of milk")
+print(f"{int(num_cups_of_coffee * coffee_beans_per_coffee)} g of coffee beans")
