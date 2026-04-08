d4a7096a6a9b5c4dadcf27f5c16dc11c2b01b6c1
Roberto Keppler <rkeppler42@gmail.com>
2026-04-08 02:11:13 -0300

    feat(easy): add the code that fulfills the third stage of the project

--- Easy/coffee-machine/coffee-machine.py
+++ Easy/coffee-machine/coffee-machine.py
@@ -2,11 +2,24 @@ water_per_coffee = 200
 milk_per_coffee = 50
 coffee_beans_per_coffee = 15
 
-
+print("Write how many ml of water the coffee machine has:")
+water_available = float(input())
+print("Write how many ml of milk the coffee machine has:")
+milk_available = float(input())
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee_available = float(input())
 print("Write how many cups of coffee you will need:")
-num_cups_of_coffee = int(input())
+cups_of_coffee_needed = int(input())
 
-print(f"For {num_cups_of_coffee} cups of coffee you will need:")
-print(f"{int(num_cups_of_coffee * water_per_coffee)} ml of water")
-print(f"{int(num_cups_of_coffee * milk_per_coffee)} ml of milk")
-print(f"{int(num_cups_of_coffee * coffee_beans_per_coffee)} g of coffee beans")
+max_coffee_per_water = int(water_available // water_per_coffee)
+max_coffe_per_milk = int(milk_available // milk_per_coffee)
+max_coffee_per_beans = int(coffee_available // coffee_beans_per_coffee)
+
+max_cups_of_coffee = min(max_coffee_per_water, max_coffe_per_milk, max_coffee_per_beans)
+
+if max_cups_of_coffee == cups_of_coffee_needed:
+    print("Yes, I can make that amount of coffee")
+elif max_cups_of_coffee > cups_of_coffee_needed:
+    print(f"Yes, I can make that amount of coffee (and even {max_cups_of_coffee - cups_of_coffee_needed} more than that)")
+else:
+    print(f"No, I can make only {max_cups_of_coffee} cups of coffee")
