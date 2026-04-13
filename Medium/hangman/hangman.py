6913bef59873a562b66e6dc713eec42b9835f859
Roberto Keppler <rkeppler42@gmail.com>
2026-04-13 02:30:23 -0300

    feat(medium): add hangman stage 1

--- Medium/hangman/hangman.py
+++ Medium/hangman/hangman.py
@@ -1,2 +1,6 @@
 print("H A N G M A N")
-print("The game will be available soon.")
+guess = input("Guess the word: > ")
+if guess == "python":
+    print("You survived!")
+else:
+    print("You lost!")
