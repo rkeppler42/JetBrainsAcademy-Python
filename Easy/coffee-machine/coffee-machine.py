d4960a91ff72ce6897f6abb221c4de03ca5dfce1
Roberto Keppler <rkeppler42@gmail.com>
2026-04-13 00:52:39 -0300

    feat(easy): add Coffee Machine project - stage 6

--- Easy/coffee-machine/coffee-machine.py
+++ Easy/coffee-machine/coffee-machine.py
@@ -1,213 +1,227 @@
-def show_current_state(current_water: int, current_milk: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> None:
-    """Print the current state of the coffee machine
+from enum import Enum
 
-    :param current_water: the current water in the machine.
-    :param current_milk: the current milk in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
+
+class CoffeeMachine:
+    """Represent a coffee machine with a state machine to handle user input.
+
+    :ivar water: the current water in the machine in ml.
+    :ivar milk: the current milk in the machine in ml.
+    :ivar coffee_beans: the current coffee beans in the machine in g.
+    :ivar disposable_cups: the current disposable cups in the machine.
+    :ivar money: the current money in the machine in $.
+    :ivar state: the current state of the machine.
     """
-    print()
-    print("The coffee machine has:")
-    print(f"{current_water} ml of water")
-    print(f"{current_milk} ml of milk")
-    print(f"{current_coffee_beans} g of coffee beans")
-    print(f"{current_disposable_cups} disposable cups")
-    print(f"${current_money} of money")
+
+    class State(Enum):
+        """Represent the possible states of the coffee machine."""
+        MAIN = "MAIN"
+        BUY = "BUY"
+        FILL_WATER = "FILL_WATER"
+        FILL_MILK = "FILL_MILK"
+        FILL_BEANS = "FILL_BEANS"
+        FILL_CUPS = "FILL_CUPS"
+        EXIT = "EXIT"
 
 
-def get_action(user_input: str, current_water: int, current_milk: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> tuple :
-    """Take input from the user and select the action to take according to it
-
-    :param user_input: the input from the user
-    :param current_water: the current water in the machine.
-    :param current_milk: the current milk in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
-    :return: a tuple of the current water, current milk, current coffee beans, current disposable cups and current money
-    """
-    match user_input:
-        case "buy":
-            return buy_action(current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money)
-        case "fill":
-            return fill_action(current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money)
-        case "take":
-            return current_water, current_milk, current_coffee_beans, current_disposable_cups, take_action(current_money)
-        case "remaining":
-            show_current_state(current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money)
-            return current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money
-        case _:
-            print("Invalid action")
-            return current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money
+    def __init__(self):
+        """Initialize the coffee machine."""
+        self.water = 400
+        self.milk = 540
+        self.coffee_beans = 120
+        self.disposable_cups = 9
+        self.money = 550
+        self.state = CoffeeMachine.State.MAIN
 
 
+    def process_input(self, input_str: str) -> str | None:
+        """Process the user input.
 
-def buy_action(current_water: int, current_milk: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> tuple :
-    """Handle the type of coffee that will be bought from the machine.
-
-    :param current_water: the current water in the machine.
-    :param current_milk: the current milk in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
-    :return: a tuple of the current water, current milk, current coffee beans, current disposable cups and current money
-    """
-    print()
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-    coffee_type = input()
-    match coffee_type :
-        case "1":
-            current_water, current_coffee_beans, current_disposable_cups, current_money = espresso(current_water, current_coffee_beans, current_disposable_cups, current_money)
-        case "2":
-            current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money = latte(current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money)
-        case "3":
-            current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money = cappuccino(current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money)
-        case "back":
-            pass
-        case _:
-            print("Invalid action")
-    return current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money
-
-
-def espresso(current_water: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> tuple :
-    """Handle the machine when the user chooses to buy an espresso.
-
-    :param current_water: the current water in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
-    :return: a tuple of the current water, current coffee beans, current disposable cups and current money
-    """
-    if current_water < 250 or current_coffee_beans < 16 or current_disposable_cups < 1:
-        deal_not_enough("espresso", current_water, current_coffee_beans, current_disposable_cups)
-    else:
-        print("I have enough resources, making you a coffee!")
-        current_water -= 250
-        current_coffee_beans -= 16
-        current_disposable_cups -= 1
-        current_money += 4
-    return current_water, current_coffee_beans, current_disposable_cups, current_money
+        :param input_str: the input to the machine.
+        :return: the output of the machine.
+        """
+        match self.state:
+            case CoffeeMachine.State.MAIN:
+                output = self._handle_main(input_str)
+                return output
+            case CoffeeMachine.State.BUY:
+                output = self._handle_buy(input_str)
+                return output
+            case CoffeeMachine.State.FILL_WATER:
+                output = self._handle_fill_water(input_str)
+                return output
+            case CoffeeMachine.State.FILL_MILK:
+                output = self._handle_fill_milk(input_str)
+                return output
+            case CoffeeMachine.State.FILL_BEANS:
+                output = self._handle_fill_beans(input_str)
+                return output
+            case CoffeeMachine.State.FILL_CUPS:
+                self._handle_fill_cups(input_str)
+            case CoffeeMachine.State.EXIT:
+                pass
 
 
-def latte(current_water: int, current_milk: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> tuple:
-    """Handle the machine when the user chooses to buy a latte.
+    def _handle_main(self, cmd: str) -> str | None:
+        """Handle the main menu.
 
-    :param current_water: the current water in the machine.
-    :param current_milk: the current milk in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
-    :return: a tuple of the current water, current milk, current coffee beans, current disposable cups and current money
-    """
-    if current_water < 350 or current_milk < 75 or current_coffee_beans < 20 or current_disposable_cups < 1:
-        deal_not_enough("latte", current_water, current_coffee_beans, current_disposable_cups, current_milk)
-    else:
-        print("I have enough resources, making you a coffee!")
-        current_water -= 350
-        current_milk -= 75
-        current_coffee_beans -= 20
-        current_disposable_cups -= 1
-        current_money += 7
-    return current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money
-
-
-def cappuccino(current_water: int, current_milk: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> tuple :
-    """Handle the machine when the user chooses to buy a cappuccino.
-
-    :param current_water: the current water in the machine.
-    :param current_milk: the current milk in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
-    :return: a tuple of the current water, current milk, current coffee beans, current disposable cups and current money
-    """
-    if current_water < 200 or current_milk < 100 or current_coffee_beans < 12 or current_disposable_cups < 1:
-        deal_not_enough("cappuccino", current_water, current_coffee_beans, current_disposable_cups, current_milk)
-    else:
-        print("I have enough resources, making you a coffee!")
-        current_water -= 200
-        current_milk -= 100
-        current_coffee_beans -= 12
-        current_disposable_cups -= 1
-        current_money += 6
-    return current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money
+        :param cmd: the input to the machine.
+        :return: the output of the machine.
+        """
+        match cmd:
+            case "remaining":
+                output = self._status()
+                return output
+            case "buy":
+                self.state = CoffeeMachine.State.BUY
+                return "\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:"
+            case "fill":
+                self.state = CoffeeMachine.State.FILL_WATER
+                return "\nWrite how many ml of water you want to add:"
+            case "take":
+                output = self._handle_take()
+                return "\n" + output
+            case "exit":
+                self.state = CoffeeMachine.State.EXIT
+        return None
 
 
-def fill_action(current_water: int, current_milk: int, current_coffee_beans: int, current_disposable_cups: int, current_money: int) -> tuple :
-    """Handle the machine when the user chooses to fill the machine.
+    def _handle_buy(self, choice: str) -> str | None:
+        """Handle the buy menu.
 
-    :param current_water: the current water in the machine.
-    :param current_milk: the current milk in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_money: the current money in the machine.
-    :return: a tuple of the current water, current milk, current coffee beans, current disposable cups and current money
-    """
-    print()
-    print("Write how many ml of water you want to add:")
-    current_water += int(input())
-    print("Write how many ml of milk you want to add:")
-    current_milk += int(input())
-    print("Write how many grams of coffee beans you want to add:")
-    current_coffee_beans += int(input())
-    print("Write how many disposable cups you want to add:")
-    current_disposable_cups += int(input())
-    return current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money
-
-
-def take_action(current_money: int) -> int:
-    """Handle the machine when the user chooses to take the money from the machine.
-
-    :param current_money: the current money in the machine.
-    :return: the current money in the machine.
-    """
-    print()
-    print(f"I gave you ${current_money}")
-    current_money = 0
-    return current_money
+        :param choice: the input to the machine.
+        :return: the output of the machine.
+        """
+        match choice:
+            case "1":
+                output = self._check_resources(250, 0, 16, 1, 4)
+                return output
+            case "2":
+                output = self._check_resources(350, 75, 20, 1, 7)
+                return output
+            case "3":
+                output = self._check_resources(200, 100, 12, 1, 6)
+                return output
+            case "back":
+                self.state = CoffeeMachine.State.MAIN
+                return None
+            case _:
+                return "Invalid choice"
 
 
-def deal_not_enough(type_of_coffee: str, current_water: int, current_coffee_beans: int, current_disposable_cups: int, current_milk=0) -> None:
-    """Handle the machine when there is less ingredient than needed.
+    def _handle_fill_water(self, val: str) -> str:
+        """Handle the fill water action.
 
-    :param type_of_coffee: the type of the coffee the user wants to make
-    :param current_water: the current water in the machine.
-    :param current_coffee_beans: the current coffee beans in the machine.
-    :param current_disposable_cups: the current disposable cups in the machine.
-    :param current_milk: the current milk in the machine.
-    """
-    if (type_of_coffee == "espresso" and current_water < 250
-        or type_of_coffee == "latte" and current_water < 350
-        or type_of_coffee == "cappuccino" and current_water < 200):
-        print("Sorry, not enough water!")
-    elif (type_of_coffee == "latte" and current_milk < 75
-        or type_of_coffee == "cappuccino" and current_milk < 100):
-        print("Sorry, not enough milk!")
-    elif (type_of_coffee == "espresso" and current_coffee_beans < 16
-        or type_of_coffee == "latte" and current_coffee_beans < 20
-        or type_of_coffee == "cappuccino" and current_coffee_beans < 12):
-        print("Sorry, not enough coffee beans!")
-    elif current_disposable_cups < 1:
-        print("Sorry, not enough disposable cups!")
+        :param val: the ml of water you want to add to the machine.
+        :return: the output of the machine.
+        """
+        val = int(val)
+        self.water += val
+        self.state = CoffeeMachine.State.FILL_MILK
+        return "Write how many ml of milk you want to add:"
+
+
+    def _handle_fill_milk(self, val: str) -> str:
+        """Handle the fill milk action.
+
+        :param val: the ml of milk you want to add to the machine.
+        :return: the output of the machine.
+        """
+        val = int(val)
+        self.milk += val
+        self.state = CoffeeMachine.State.FILL_BEANS
+        return "Write how many grams of coffee beans you want to add:"
+
+
+    def _handle_fill_beans(self, val: str) -> str:
+        """Handle the fill beans action.
+
+        :param val: the g of coffee beans you want to add to the machine.
+        :return: the output of the machine.
+        """
+        val = int(val)
+        self.coffee_beans += val
+        self.state = CoffeeMachine.State.FILL_CUPS
+        return "Write how many disposable cups you want to add:"
+
+
+    def _handle_fill_cups(self, val: str) -> None:
+        """Handle the fill cups action.
+
+        :param val: the quantity of disposable cups you want to add to the machine.
+        """
+        val = int(val)
+        self.disposable_cups += val
+        self.state = CoffeeMachine.State.MAIN
+
+
+    def _check_resources(self, w: int, m: int, b: int, cups: int, price: int) -> str | None:
+        """Check if the machine has enough resources to prepare the coffee.
+
+        :param w: the water you need to do the type of coffee selected;
+        :param m: the milk you need to do the type of coffee selected;
+        :param b: the beans you need to do the type of coffee selected;
+        :param cups: the cups you need to do the type of coffee selected;
+        :param price: the price of the coffee you want to buy;
+        :return: the output of the machine.
+        """
+        self.state = CoffeeMachine.State.MAIN
+        if self.water >= w and self.milk >= m and self.coffee_beans >= b and self.disposable_cups >= cups:
+            self.water -= w
+            self.milk -= m
+            self.coffee_beans -= b
+            self.disposable_cups -= cups
+            self.money += price
+            return "\nI have enough resources, making you a coffee!"
+
+        elif self.water < w:
+            return "\nSorry, not enough water!"
+        elif self.milk < m:
+            return "\nSorry, not enough milk!"
+        elif self.coffee_beans < b:
+            return "\nSorry, not enough coffee beans!"
+        elif self.disposable_cups < cups:
+            return "\nSorry, not enough disposable cups!"
+        return None
+
+
+    def _handle_take(self) -> str:
+        """Handle the take action.
+
+        :return: the output of the machine.
+        """
+        current_money = self.money
+        self.money = 0
+        self.state = CoffeeMachine.State.MAIN
+        return f"I gave you ${current_money}"
+
+
+    def _status(self) -> str:
+        """Handle the remaining action.
+
+        :return: the output of the machine.
+        """
+        return (f"\nThe coffee machine has:\n"
+                f"{self.water} ml of water\n"
+                f"{self.milk} ml of milk\n"
+                f"{self.coffee_beans} g of coffee beans\n"
+                f"{self.disposable_cups} disposable cups\n"
+                f"${self.money} of money\n")
 
 
 def main():
     """The main function of the coffee machine.
     """
-    current_water = 400
-    current_milk = 540
-    current_coffee_beans = 120
-    current_disposable_cups = 9
-    current_money = 550
+    coffee_machine = CoffeeMachine()
 
-    while True:
-        print("Write action (buy, fill, take, remaining, exit):")
-        action = input()
-        if action == "exit":
-            break
-        current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money = get_action(action, current_water, current_milk, current_coffee_beans, current_disposable_cups, current_money)
-        print()
+    while coffee_machine.state != CoffeeMachine.State.EXIT:
+        if coffee_machine.state == CoffeeMachine.State.MAIN:
+            print("Write action (buy, fill, take, remaining, exit):")
+        user_input = input()
+        output = coffee_machine.process_input(user_input)
+
+        if output:
+            print(output)
+
 
 
 if __name__ == "__main__":
