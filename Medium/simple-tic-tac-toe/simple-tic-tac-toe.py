91a5a3303975c42da3aa43641820f94819626e2d
Roberto Keppler <rkeppler42@gmail.com>
2026-05-12 21:48:43 -0300

    feat(medium): add simple-tic-tac-toe stage 2

--- Medium/simple-tic-tac-toe/simple-tic-tac-toe.py
+++ Medium/simple-tic-tac-toe/simple-tic-tac-toe.py
@@ -1,9 +1,13 @@
+game_state = input()
+
 grid = [
-    ["X", "O", "X"],
-    ["O", "X", "O"],
-    ["X", "X", "O"]
+    [x for x in game_state[0:3]],
+    [x for x in game_state[3:6]],
+    [x for x in game_state[6:9]],
 ]
 
+print("---------")
 for row in grid:
-    line = " ".join(row)
+    line = "| " + " ".join(row) + " |"
     print(line)
+print("---------")
